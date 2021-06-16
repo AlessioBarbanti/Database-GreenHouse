035b62f142a8fb1f5687279fbacd2502c90b11f2
Angeli tos <angelo.tinti.30@gmail.com>
2021-06-16 13:29:36 +0200

    Add new version of GreenHouse_App with name without space

---
+++ GreenHouse_App/GreenHouse_App/FormRicercatore.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GreenHouse_App
+{
+    public partial class FormRicercatore : Form
+    {
+        public FormRicercatore()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FormRicercatore2 f2 = new FormRicercatore2();
+            f2.ShowDialog();
+        }
+    }
+}
---
+++ GreenHouse_App/GreenHouse_App/FormRicercatore2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GreenHouse_App
+{
+    public partial class FormRicercatore2 : Form
+    {
+        public FormRicercatore2()
+        {
+            InitializeComponent();
+        }
+
+        private void Inserisci_Click(object sender, EventArgs e)
+        {
+            //TODO
+            Close();
+        }
+
+        private void Annulla_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
