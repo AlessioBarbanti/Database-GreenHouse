e2606422b4d2049d8beab115fa06547df7f51625
Alessiotb <alessiokyra@gmail.com>
2021-06-20 16:58:32 +0200

    Fixed minor problems in Gui

--- GreenHouse_App/GreenHouse_App/FormGestoreStruttura.Designer.cs
+++ GreenHouse_App/GreenHouse_App/FormGestoreStruttura.Designer.cs
@@ -29,9 +29,12 @@ namespace GreenHouse_App
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormGestoreStruttura));
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.RevisioniInScadenza = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxIDStrutturaDiRiferimento = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -39,12 +42,28 @@ namespace GreenHouse_App
             // 
             this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.Disable;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(217, 12);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.ShowCellErrors = false;
+            this.dataGridView1.ShowCellToolTips = false;
+            this.dataGridView1.ShowEditingIcon = false;
+            this.dataGridView1.ShowRowErrors = false;
             this.dataGridView1.Size = new System.Drawing.Size(779, 491);
-            this.dataGridView1.TabIndex = 15;
+            this.dataGridView1.TabIndex = 3;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // RevisioniInScadenza
@@ -67,11 +86,29 @@ namespace GreenHouse_App
             this.RevisioniInScadenza.UseVisualStyleBackColor = true;
             this.RevisioniInScadenza.Click += new System.EventHandler(this.RevisioniInScadenza_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 13);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "ID Struttura di riferimento: ";
+            // 
+            // textBoxIDStrutturaDiRiferimento
+            // 
+            this.textBoxIDStrutturaDiRiferimento.Location = new System.Drawing.Point(64, 120);
+            this.textBoxIDStrutturaDiRiferimento.Name = "textBoxIDStrutturaDiRiferimento";
+            this.textBoxIDStrutturaDiRiferimento.Size = new System.Drawing.Size(75, 20);
+            this.textBoxIDStrutturaDiRiferimento.TabIndex = 19;
+            // 
             // FormGestoreStruttura
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1008, 515);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxIDStrutturaDiRiferimento);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.RevisioniInScadenza);
             this.MaximizeBox = false;
@@ -80,6 +117,7 @@ namespace GreenHouse_App
             this.Text = "Gestore Struttura";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -87,5 +125,7 @@ namespace GreenHouse_App
 
         private System.Windows.Forms.Button RevisioniInScadenza;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxIDStrutturaDiRiferimento;
     }
 }
--- GreenHouse_App/GreenHouse_App/FormGestoreStruttura.cs
+++ GreenHouse_App/GreenHouse_App/FormGestoreStruttura.cs
@@ -24,8 +24,8 @@ namespace GreenHouse_App
         {
 
             var foo = from a in db.MACCHINARI
-                      where (a.DataRevisione - DateTime.Now).Days < 10 && (DateTime.Now - a.DataRevisione).Days < 0
-                      select a;
+                      where (a.DataRevisione - DateTime.Now).Days < 10 && (DateTime.Now - a.DataRevisione).Days < 0 && (Convert.ToString(a.IDStruttura) == textBoxIDStrutturaDiRiferimento.Text)
+                      select new {a.Marca, a.Modello, a.Targa, a.DataRevisione};
 
             dataGridView1.DataSource = foo;
         }
