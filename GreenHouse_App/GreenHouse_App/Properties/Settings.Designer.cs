41acc8c022215e561d49efb37ace515be1f37ef6
Alessiotb <alessiokyra@gmail.com>
2021-06-21 18:55:11 +0200

    Cleaned Repo, added 'da consegnare' folder

--- GreenHouse_App/GreenHouse_App/Properties/Settings.Designer.cs
+++ GreenHouse_App/GreenHouse_App/Properties/Settings.Designer.cs
@@ -55,5 +55,16 @@ namespace GreenHouse_App.Properties {
                 return ((string)(this["Greenhouse_DatabaseConnectionString"]));
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=GreenhouseDatabase;Integrated " +
+            "Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False")]
+        public string GreenhouseDatabaseConnectionString {
+            get {
+                return ((string)(this["GreenhouseDatabaseConnectionString"]));
+            }
+        }
     }
 }
